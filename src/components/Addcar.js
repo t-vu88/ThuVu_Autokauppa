2aad073a28e00319d6f53f67f74f5ac93d5ba285
Thu Trang Vu <vuthutrang2811@gmail.com>
2023-11-10 18:18:52 +0200

    Kaikki lähdekoodit on tarkistettu

--- src/components/Addcar.js
+++ src/components/Addcar.js
@@ -5,7 +5,6 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
-import { PropaneSharp } from "@mui/icons-material";
 
 
 export default function AddCar(props){
